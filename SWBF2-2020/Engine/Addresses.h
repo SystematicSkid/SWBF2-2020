dfd2e15a98079846e8da62c23eb5e8decfe169e7
SystematicSkid <superseb123abc@gmail.com>
2020-05-20 11:00:11 -0500

    Formatting fix

--- SWBF2-2020/Engine/Addresses.h
+++ SWBF2-2020/Engine/Addresses.h
@@ -32,8 +32,8 @@ namespace Engine
 
 			namespace Offsets
 			{
-				DWORD CONST GameCamera = 0x0024;
-				DWORD CONST Position = 0x003C;
+				DWORD CONST GameCamera	= 0x0024;
+				DWORD CONST Position	= 0x003C;
 			}
 		}
 
@@ -41,8 +41,8 @@ namespace Engine
 		{
 			namespace Offsets
 			{
-				DWORD CONST MatView = 0x70;
-				DWORD CONST MatProjection = 0xB0;
+				DWORD CONST MatView			= 0x70;
+				DWORD CONST MatProjection	= 0xB0;
 			}
 
 			namespace Functions
@@ -55,15 +55,15 @@ namespace Engine
 		{
 			namespace Offsets
 			{
-				DWORD CONST Id = 0x0008;
-				DWORD CONST Name = 0x000C;
-				DWORD CONST NumReinforcements = 0x0028;
-				DWORD CONST NumCharacters = 0x0038;
-				DWORD CONST Characters = 0x0040;
-				DWORD CONST NumClasses = 0x0048;
-				DWORD CONST Classes = 0x0050;
-				DWORD CONST TeamId = 0x64;
-				DWORD CONST Relationship = 0x0088;
+				DWORD CONST Id					= 0x0008;
+				DWORD CONST Name				= 0x000C;
+				DWORD CONST NumReinforcements	= 0x0028;
+				DWORD CONST NumCharacters		= 0x0038;
+				DWORD CONST Characters			= 0x0040;
+				DWORD CONST NumClasses			= 0x0048;
+				DWORD CONST Classes				= 0x0050;
+				DWORD CONST TeamId				= 0x64;
+				DWORD CONST Relationship		= 0x0088;
 			}
 
 			namespace VMTable
@@ -76,12 +76,12 @@ namespace Engine
 		{
 			namespace Offsets
 			{
-				DWORD CONST Name = 0x0030;
-				DWORD CONST TeamId = 0x0134;
-				DWORD CONST Team = 0x0138;
-				DWORD CONST CharacterClass = 0x0140;
-				DWORD CONST Entity = 0x0148;
-				DWORD CONST Pad = 0x1AC;
+				DWORD CONST Name			= 0x0030;
+				DWORD CONST TeamId			= 0x0134;
+				DWORD CONST Team			= 0x0138;
+				DWORD CONST CharacterClass	= 0x0140;
+				DWORD CONST Entity			= 0x0148;
+				DWORD CONST Pad				= 0x1AC;
 			}
 		}
 
@@ -90,8 +90,8 @@ namespace Engine
 			DWORD CONST Instance = ModuleGame + 0x1A30330;
 			namespace Offsets
 			{
-				DWORD CONST NumCharacters = 0x0000;
-				DWORD CONST CharacterArray = 0x0004;
+				DWORD CONST NumCharacters	= 0x0000;
+				DWORD CONST CharacterArray	= 0x0004;
 			}
 		}
 
@@ -99,11 +99,11 @@ namespace Engine
 		{
 			namespace Offsets
 			{
-				DWORD CONST NextClass = 0x0008;
-				DWORD CONST PreviousClass = 0x000C;
-				DWORD CONST ClassId = 0x001C;
-				DWORD CONST Name = 0x0020;
-				DWORD CONST Hitboxes = 0x0ABC;
+				DWORD CONST NextClass		= 0x0008;
+				DWORD CONST PreviousClass	= 0x000C;
+				DWORD CONST ClassId			= 0x001C;
+				DWORD CONST Name			= 0x0020;
+				DWORD CONST Hitboxes		= 0x0ABC;
 			}
 		}
 
@@ -111,9 +111,9 @@ namespace Engine
 		{
 			namespace Offsets
 			{
-				DWORD CONST PlayerController = 0x00C8;
-				DWORD CONST Character = 0x00CC;
-				DWORD CONST Position = 0x00DC;
+				DWORD CONST PlayerController	= 0x00C8;
+				DWORD CONST Character			= 0x00CC;
+				DWORD CONST Position			= 0x00DC;
 			}
 		}
 	}
